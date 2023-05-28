5252960de0989763d99d75974eef5c0a730415b6
AppleHowMuch <SecreTary_RC@hotmail.com>
2023-05-29 00:27:09 +0800

    change index

---
+++ StudentInfo/js/student.js
@@ -0,0 +1,23 @@
+// Search
+const searchInput = document.querySelector("#searchinput");
+const searchbtn = document.querySelector("#searchbtn");
+
+let arr =[];
+
+function showall(){
+    let res = [];
+    
+    fetch("http://localhost:8080/" ,{
+        method:"POST",
+        headers:{
+            Con
+        }
+    })
+
+
+}
+
+
+searchbtn.addEventListener('click' , function(){
+    
+})
